09380ab5883dbdd25df236c319bc5e5c4de7115e
Brenda Silva <brenda.amsilva@gmail.com>
2025-08-25 15:35:10 -0300

    v2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,18 @@ class QuestionsController < ApplicationController
 def ask
 end
 
+def answer
+  @input = params[:question]
+  if @input == "I am going to work"
+    @response = "Great!"
+  elsif @input.include?("?")
+    @response = "Silly question, get dressed and go to work!"
+  else
+    @response = "I don't care, get dressed and go to work!"
+  end
+
+  puts @response
+
+end
+
 end
